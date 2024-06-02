0b1a9990cce1ca706ab878562f6de068ade4f084
lysShub <lysShub@gmail.com>
2024-06-03 03:45:19 +0800

    add code

--- nodes/forward/forward_test.go
+++ nodes/forward/forward_test.go
@@ -4,19 +4,16 @@
 package forward_test
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/lysShub/anton-planet-accelerator/nodes/forward"
+	"github.com/lysShub/netkit/debug"
 	"github.com/stretchr/testify/require"
 )
 
 func TestXxxx(t *testing.T) {
-	// var ip = netip.MustParseAddr("172.241.71.116")
-
-	// s := time.Now()
-	// country, err := GetCountry(ip)
-	// require.NoError(t, err)
-	// fmt.Println(country, time.Since(s))
+	fmt.Println(debug.Debug())
 
 	config := &forward.Config{
 		MaxRecvBuffSize: 1536,
